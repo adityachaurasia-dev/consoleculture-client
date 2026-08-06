8b3016e13b687acf163977bf404896b40fffefce
Aditya Chaurasia <adityachaurasia.dev@gmail.com>
2026-08-07 01:25:46 +0530

    svg error fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  turbopack: {},
   allowedDevOrigins: ["192.168.1.13"],
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      use: ["@svgr/webpack"],
-    });
-
-    return config;
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
   },
 };
 
